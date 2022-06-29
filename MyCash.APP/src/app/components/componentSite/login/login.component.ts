41f6b3b107f20959cee1da5752c5cc5f8c282b5c
Cleydson Fernando <cleydsonfernando@gmal.com>
2022-06-29 17:47:08 -0300

    “ajustes”

--- MyCash.APP/src/app/components/componentSite/login/login.component.ts
+++ MyCash.APP/src/app/components/componentSite/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Usuario } from '../cadastro/usuario';
 import { UsuarioService } from '../cadastro/usuario.service';
@@ -11,11 +12,17 @@ import { UsuarioService } from '../cadastro/usuario.service';
 export class LoginComponent implements OnInit {
   usuariosLista: Usuario[];
   email: string;
-  password: Number;
+  password: string;
+  // formulario: any;
 
   constructor(private usuarioService: UsuarioService, private router : Router) { }
 
   ngOnInit(): void {
+    //   this.formulario = new FormGroup({
+    //   Email: new FormControl(null,[Validators.required, Validators.email]),
+    //   Senha : new FormControl(null, [Validators.required])
+    // })
+    // this.registerSuccess = false
 
   }
 
@@ -24,8 +31,8 @@ export class LoginComponent implements OnInit {
     this.usuarioService.PegarTodos().subscribe((data) => {
       this.usuariosLista = data;
       for (let i = 0; i < this.usuariosLista.length; i++) {
-        if (this.usuariosLista[i].email == this.email &&
-        this.usuariosLista[i].senha == this.password){
+        if (this.usuariosLista[i].email== this.email &&
+        this.usuariosLista[i].senha== this.password){
         this.router.navigate(['/dashboard']);
 
 
