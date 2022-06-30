59ad3915314bdb92153cdd0cbf504ccdea456319
Cleydson Fernando <cleydsonfernando@gmal.com>
2022-06-30 15:07:32 -0300

    logins

--- MyCash.APP/src/app/components/componentSite/login/login.component.ts
+++ MyCash.APP/src/app/components/componentSite/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Usuario } from '../cadastro/usuario';
 import { UsuarioService } from '../cadastro/usuario.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -10,14 +11,18 @@ import { UsuarioService } from '../cadastro/usuario.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  usuariosLista: Usuario[];
-  email: string;
-  password: string;
-  // formulario: any;
+  usuariosLista: any =[];
+  email: '';
+  password: '';
+  userValido!: boolean;
+  userInvalido!: boolean;
+  loginLoad: boolean = false
 
   constructor(private usuarioService: UsuarioService, private router : Router) { }
 
   ngOnInit(): void {
+
+    this.userValido = false;
     //   this.formulario = new FormGroup({
     //   Email: new FormControl(null,[Validators.required, Validators.email]),
     //   Senha : new FormControl(null, [Validators.required])
@@ -27,14 +32,26 @@ export class LoginComponent implements OnInit {
   }
 
   Logar(){
-    console.log(this.email+'-'+this.password)
+    alert (this.email)
     this.usuarioService.PegarTodos().subscribe((data) => {
       this.usuariosLista = data;
-      for (let i = 0; i < this.usuariosLista.length; i++) {
-        if (this.usuariosLista[i].email== this.email &&
-        this.usuariosLista[i].senha== this.password){
-        this.router.navigate(['/dashboard']);
+      for (let i = 0; i < data.length; i++) {
+
+        if
+
+        (this.usuariosLista[i].email== this.email &&
+        this.usuariosLista[i].senha== this.password)
+        {
+          this.userValido = true;
+          this.userInvalido = false;
+          this.loginLoad = true;
+          setTimeout(() => {
+            this.router.navigate(['dashboard']);
+          }, 2000);
         }
+      }
+      if (this.userValido == false) {
+        this.userInvalido = true;
       }
     });
   }
